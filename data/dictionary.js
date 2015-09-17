14f52a2eef5ff21dfe4c051f2b9aad1370df6bc3
garyLoveSuwye <987683297@qq.com>
2015-09-17 10:09:21 +0800

    就差我了！

---
+++ data/dictionary.js
@@ -0,0 +1,83 @@
+﻿window.worldObj = {
+    "6Asian": {
+        "urls": [{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/Forever_英举love圈圈.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/在一起_英举love圈圈.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/大二时光_英举love圈圈.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/执子之手_英举love圈圈.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/执子之手_英举love圈圈.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/深圳_达姐.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/天长地久_英举love圈圈.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/河南商丘_张耿.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/北京_波波.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/山东_运仔.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/北京_舒琪.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/广州_张想.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/德福_杭州.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/苏州_进哥.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/冲哥_北京.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/玉兰姐_广州.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/广州_伤心的小白.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/因为你娶了贤惠的她_我只能祝福.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/生娃的事需要抓紧啊_小白.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/给你我的心_Panda.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/周启_北京.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/赵宁_北京.jpg"
+        },{
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/28Hengshui_116_38/image/执子之手_英举love圈圈.jpg"
+        }],
+        "urlJson": "./data/3worldwishes/6Asian/info.json"
+    },
+    "0Asian": {
+        "urls": [   {
+            "type": "image",
+            "url": "./data/3worldwishes/0Asian/06BeiJin_116_40/image/北京中科院_安兴乐.jpg"
+        }],
+        "urlJson": "./data/3worldwishes/6Asian/info.json"
+    }
+}
+window.worldArr = [ "6Asian", "1NorthAmerica", "0Asian", "2SouthAmerican", "3Africa","4Europe", "5Oceania"]
